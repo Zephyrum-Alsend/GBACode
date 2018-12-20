44f17ee79b113a426ce4c7131f043f4d27ac4bc5
Zephyrum-Alsend <isfaryl@gmail.com>
2018-12-20 13:46:19 -0500

    template
    A base template to copy&paste in order to get projects started.  The code already present makes the background red.

---
+++ template/source/template.c
@@ -0,0 +1,24 @@
+typedef unsigned char      uint8;
+typedef unsigned short     uint16;
+typedef unsigned int          uint32;
+
+#define REG_DISPLAYCONTROL	*((volatile uint32*)(0x04000000))
+#define VIDEOMODE_3     	               0x0003
+#define BGMODE_2 			       0x0400
+
+#define SCREENBUFFER                    ((volatile uint16*)0x06000000)
+#define SCREEN_W                            240
+#define SCREEN_H                            160
+
+int main()
+{
+    REG_DISPLAYCONTROL = VIDEOMODE_3 | BGMODE_2;
+
+	for (int i = 0; i < SCREEN_W * SCREEN_H; ++i)
+	{
+		SCREENBUFFER[i] = 0x001F;
+	}
+
+    while(1){}
+    return 0;
+}   
