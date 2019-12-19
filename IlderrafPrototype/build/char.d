ea86a9ffd1085235c6be8953920896f31a4237a6
Zephyrum-Alsend <isfaryl@gmail.com>
2019-12-19 01:46:00 -0500

    Delete char.d

--- IlderrafPrototype/build/char.d
+++
@@ -1,7 +0,0 @@
-char.o: E:/Ilderraf/ZeCode/IlderrafPrototype/source/char.cpp \
- E:/Ilderraf/ZeCode/IlderrafPrototype/include/char.h \
- E:/Ilderraf/ZeCode/IlderrafPrototype/include/gba.h
-
-E:/Ilderraf/ZeCode/IlderrafPrototype/include/char.h:
-
-E:/Ilderraf/ZeCode/IlderrafPrototype/include/gba.h:
